b1b559f3b1c41423ffbb1b0a0965fec8ffc2fac0
Дмитрий Шурашов <shurashovd@yandex.ru>
2022-10-14 15:25:40 +0300

    v1.3.101

--- site/components/NavCatalog.tsx
+++ site/components/NavCatalog.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { Button, Col, Collapse, Nav, Row, Stack, Tab } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { resetFilters } from "../application/filtersSlice";
@@ -8,6 +8,8 @@ import { hideNavCatalog } from "../application/navCatalogSlice";
 const NavCatalog = () => {
     const { show } = useAppSelector(state => state.navCatalogSlice)
 	const { categories } = useAppSelector(state => state.categoriesSlice)
+	const [pane, setPane] = useState("0")
+	const candidatePane = useRef<string|undefined>()
 	const navigate = useNavigate()
 	const dispatch = useAppDispatch()
 
@@ -15,11 +17,27 @@ const NavCatalog = () => {
 		dispatch(resetFilters())
 		navigate(to)
 		dispatch(hideNavCatalog())
+		setPane("0")
+	}
+
+	const hoverHandler = (key: string) => {
+		candidatePane.current = key
+		setTimeout(() => {
+			if ( candidatePane.current === key ) {
+				setPane(key)
+			}
+		}, 250)
+		
+	}
+
+	const leaveHandler = () => {
+		candidatePane.current = undefined
 	}
 
 	const handler = useCallback((event: any) => {
 		if ( !event.path.some((item: any) => item.classList?.contains('nav-catalog-container')) ) {
 			dispatch(hideNavCatalog())
+			setPane("0")
 		}
 	}, [])
 
@@ -40,13 +58,17 @@ const NavCatalog = () => {
     return (
 		<Collapse in={show}>
 			<div className="position-absolute top-0 start-0 end-0 bottom-0 nav-catalog-container">
-				<Tab.Container>
-					<Row className="m-0 p-0" style={{ borderBottom: '2px solid #ab9a9a' }}>
+				<Tab.Container activeKey={pane}>
+					<Row className="m-0 p-0" style={{ borderBottom: "2px solid #ab9a9a" }}>
 						<Col xs={3} className="p-0 py-4" style={{ backgroundColor: "#f1f1f1" }}>
 							<Nav variant="pills" className="flex-column" id="navCatalogTab">
 								{categories.map(({ _id, title }) => (
-									<Nav.Item key={_id?.toString()}>
-										<Nav.Link eventKey={_id?.toString()} className="ps-6 py-4 text-uppercase">
+									<Nav.Item key={_id?.toString()} onMouseOver={() => hoverHandler(_id?.toString())} onMouseLeave={leaveHandler}>
+										<Nav.Link
+											eventKey={_id?.toString()}
+											onClick={() => clickHandler(`/category/${_id?.toString()}/[]`)}
+											className="ps-6 py-4 text-uppercase"
+										>
 											{title}
 										</Nav.Link>
 									</Nav.Item>
