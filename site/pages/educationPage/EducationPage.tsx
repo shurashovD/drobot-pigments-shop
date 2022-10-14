96b3b4547a62691f11f3e2e69c8f2bb1031a1ff6
Дмитрий Шурашов <shurashovd@yandex.ru>
2022-10-14 20:30:48 +0300

    v1.3.102

--- site/pages/educationPage/EducationPage.tsx
+++ site/pages/educationPage/EducationPage.tsx
@@ -18,14 +18,6 @@ const EducationPage = () => {
 				<Row xs={2} lg={3} className="g-4">
 					<Col>
 						<Item
-							folder="coach"
-							title="Тренер Drobot pigments"
-							url="https://drobot-online-academy.ru/trener_drobot_pigments"
-							widthToHeight={263 / 168}
-						/>
-					</Col>
-					<Col>
-						<Item
 							folder="baseAfterBase"
 							title="курс “База после базы”"
 							url="https://drobot-online-academy.ru/bpb_predzayavki"
@@ -43,7 +35,7 @@ const EducationPage = () => {
 					<Col>
 						<Item
 							folder="sketch"
-							title="Курс “про эскиз”"
+							title="Курс “Дробот про эскиз”"
 							url="https://drobot-online-academy.ru/drobot_pro_eskiz"
 							widthToHeight={263 / 168}
 						/>
@@ -53,6 +45,47 @@ const EducationPage = () => {
 							folder="brows"
 							title="Курс “роскошь бровей”"
 							url="https://drobot-online-academy.ru/roskosh_brovey"
+							widthToHeight={263 / 168}
+						/>
+					</Col>
+					<Col>
+						<Item folder="lazer" title='Курс "Лазерное удаление"' url="https://drobot-online-academy.ru/lu" widthToHeight={263 / 168} />
+					</Col>
+					<Col>
+						<Item folder="monaliza" title='Курс "MonaLiza 7"' url="https://drobot-online-academy.ru/lazer" widthToHeight={263 / 168} />
+					</Col>
+					<Col>
+						<Item
+							folder="magicRemover"
+							title='Курс "Ремувер MAGIC"'
+							url="https://drobot-online-academy.ru/magic_cource"
+							widthToHeight={263 / 168}
+						/>
+					</Col>
+					<Col>
+						<Item
+							folder="mezhresnichka"
+							title='Курс "межресничная зона"'
+							url="https://drobot-online-academy.ru/mezhresnichka"
+							widthToHeight={263 / 168}
+						/>
+					</Col>
+					<Col>
+						<Item
+							folder="feathering"
+							title='Курс "теневая растушевка"'
+							url="https://drobot-online-academy.ru/tenevaya_rastushevka"
+							widthToHeight={263 / 168}
+						/>
+					</Col>
+					<Col>
+						<Item folder="sanpin" title='Курс "СанПиН"' url="https://drobot-online-academy.ru/sanpin_for_pm" widthToHeight={263 / 168} />
+					</Col>
+					<Col>
+						<Item
+							folder="tatoo"
+							title='Курс "Мини тату"'
+							url="https://drobot-online-academy.ru/minitattoo_for_pmu_masters"
 							widthToHeight={263 / 168}
 						/>
 					</Col>
