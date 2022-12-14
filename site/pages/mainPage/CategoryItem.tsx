29bfc652d29e137632ecbdea490277fcbe685f0d
Дмитрий Шурашов <shurashovd@yandex.ru>
2022-12-14 08:06:44 +0300

    Динамические подкатегории на главной

--- site/pages/mainPage/CategoryItem.tsx
+++ site/pages/mainPage/CategoryItem.tsx
@@ -1,32 +1,38 @@
 import { ICategory } from '../../../shared/index'
 import { FC } from 'react'
 import ImageComponent from '../../components/ImageComponent'
-import { NavLink, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 interface IProps {
-    category?: ICategory
+	category?: ICategory
 	widthToHeight: number
 	title: string
-	description?: string 
+	description?: string
 }
 
 const CategoryItem: FC<IProps> = ({ category, widthToHeight, title, description }) => {
 	const navigate = useNavigate()
 
-	const to = category?.frontEndKey === "pigments" ? `/pigments/${category?._id?.toString()}` : `/category/${category?._id?.toString()}/[]`
+	const clickHandler = () => {
+		const id = category?._id.toString()
+		if ( !id ) {
+			return
+		}
+		const hasSubCategories = !!category?.subCategories.length
+		const path = hasSubCategories ? "parent-category" : "category"
+		const to = `/${path}/${id}/[]`
+		navigate(to)
+	}
 
-    return (
-		<div className="position-relative h-100 category-card" onClick={() => navigate(to)}>
+	return (
+		<div className="position-relative h-100 category-card" onClick={() => clickHandler()}>
 			<ImageComponent src={category?.photo[0] || "/static/"} widthToHeight={widthToHeight} />
-			<NavLink
-				className="w-100 p-2 px-xl-4 bottom-0 start-0 end-0 d-flex align-items-center category-card__label"
-				to={to}
-			>
+			<div className="w-100 p-2 px-xl-4 bottom-0 start-0 end-0 d-flex align-items-center category-card__label">
 				<div>
 					<span className="text-uppercase nowrap">{title}</span>
-					<span>{" "}{description}</span>
+					<span> {description}</span>
 				</div>
-			</NavLink>
+			</div>
 		</div>
 	)
 }
