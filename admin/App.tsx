a68dfb2612f4794c79e3469971f74da8950f8c5a
Дмитрий Шурашов <shurashovd@yandex.ru>
2022-12-15 08:33:33 +0300

    Админка разбита

--- admin/App.tsx
+++ admin/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { lazy, Suspense, useEffect } from "react"
 import { Container } from "react-bootstrap"
 import { Navigate, Route, Routes } from "react-router-dom"
 import { useAppDispatch } from "./application/hooks"
@@ -6,20 +6,20 @@ import { useGetOrdersQuery } from "./application/order.service"
 import { setOrders } from "./application/ordersSlice"
 import AlertComponent from "./components/AlertComponent"
 import HeaderComponent from "./components/HeaderComponent"
-import AmoPage from "./pages/amoPage/AmoPage"
-import CashbackPage from "./pages/cashbackPage/CashbackPage"
-import CategoryPage from "./pages/categoryPage/CategoryPage"
-import ClientPage from "./pages/clientPage/ClientPage"
-import DebitesPage from "./pages/debitesPage/DebitesPage"
-import HooksPage from "./pages/hooksPage/HooksPage"
-import LoyaltyPage from "./pages/loyaltyPage/LoyaltyPage"
-import MoySkladPage from "./pages/moySkladPage/MoySkladPage"
-import OrderPage from "./pages/ordersPage/OrderPage"
-import OrdersPage from "./pages/ordersPage/OrdersPage"
-import ProductPage from "./pages/productPage/ProductPage"
-import PromocodePage from "./pages/promocodePage/PromocodePage"
-import UsersPage from "./pages/usersPage/UsersPage"
-import SiteCatalogPage from "./pages/siteCatalogPage/SiteCatalogPage"
+const AmoPage = lazy(() => import("./pages/amoPage/AmoPage"))
+const CashbackPage = lazy(() => import("./pages/cashbackPage/CashbackPage"))
+const CategoryPage = lazy(() => import("./pages/categoryPage/CategoryPage"))
+const ClientPage = lazy(() => import("./pages/clientPage/ClientPage"))
+const DebitesPage = lazy(() => import("./pages/debitesPage/DebitesPage"))
+const HooksPage = lazy(() => import("./pages/hooksPage/HooksPage"))
+const LoyaltyPage = lazy(() => import("./pages/loyaltyPage/LoyaltyPage"))
+const MoySkladPage = lazy(() => import("./pages/moySkladPage/MoySkladPage"))
+const OrderPage = lazy(() => import("./pages/ordersPage/OrderPage"))
+const OrdersPage = lazy(() => import("./pages/ordersPage/OrdersPage"))
+const ProductPage = lazy(() => import("./pages/productPage/ProductPage"))
+const SiteCatalogPage = lazy(() => import("./pages/siteCatalogPage/SiteCatalogPage"))
+const PromocodePage = lazy(() => import("./pages/promocodePage/PromocodePage"))
+const UsersPage = lazy(() => import("./pages/usersPage/UsersPage"))
 
 const App = () => {
 	const { data, isSuccess } = useGetOrdersQuery({}, { refetchOnMountOrArgChange: true })
@@ -37,23 +37,128 @@ const App = () => {
 			<HeaderComponent />
 			<Container fluid className="pt-4">
 				<Routes>
-					<Route path="/admin/moy-sklad" element={<MoySkladPage />} />
+					<Route
+						path="/admin/moy-sklad"
+						element={
+							<Suspense>
+								<MoySkladPage />
+							</Suspense>
+						}
+					/>
 					<Route path="/admin/moy-sklad/:id" element={<MoySkladPage />} />
-					<Route path="/admin/categories" element={<SiteCatalogPage />} />
-					<Route path="/admin/category-subcategories/:id" element={<SiteCatalogPage />} />
-					<Route path="/admin/category-products/:id" element={<CategoryPage />} />
-					<Route path="/admin/product/:id" element={<ProductPage />} />
-					<Route path="/admin/orders" element={<OrdersPage />} />
-					<Route path="/admin/orders/:id" element={<OrderPage />} />
-					<Route path="/admin/hooks" element={<HooksPage />} />
-					<Route path="/admin/amo" element={<AmoPage />} />
-					<Route path="/admin/users" element={<UsersPage />} />
-					<Route path="/admin/loyalty" element={<LoyaltyPage />} />
-					<Route path="/admin/client/:id" element={<ClientPage />} />
-					<Route path="/admin/cashback" element={<CashbackPage />} />
-					<Route path="/admin/promocode/:id" element={<PromocodePage />} />
-					<Route path="/admin/client/debites/:id" element={<DebitesPage />} />
-					<Route path="*" element={<Navigate to="/admin/moy-sklad" />} />
+					<Route
+						path="/admin/categories"
+						element={
+							<Suspense>
+								<SiteCatalogPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/category-subcategories/:id"
+						element={
+							<Suspense>
+								<SiteCatalogPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/category-products/:id"
+						element={
+							<Suspense>
+								<CategoryPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/product/:id"
+						element={
+							<Suspense>
+								<ProductPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/orders"
+						element={
+							<Suspense>
+								<OrdersPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/orders/:id"
+						element={
+							<Suspense>
+								<OrderPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/hooks"
+						element={
+							<Suspense>
+								<HooksPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/amo"
+						element={
+							<Suspense>
+								<AmoPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/users"
+						element={
+							<Suspense>
+								<UsersPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/loyalty"
+						element={
+							<Suspense>
+								<LoyaltyPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/client/:id"
+						element={
+							<Suspense>
+								<ClientPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/cashback"
+						element={
+							<Suspense>
+								<CashbackPage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/promocode/:id"
+						element={
+							<Suspense>
+								<PromocodePage />
+							</Suspense>
+						}
+					/>
+					<Route
+						path="/admin/client/debites/:id"
+						element={
+							<Suspense>
+								<DebitesPage />
+							</Suspense>
+						}
+					/>
+					<Route path="*" element={<Navigate to="/admin" />} />
 				</Routes>
 			</Container>
 		</Container>
